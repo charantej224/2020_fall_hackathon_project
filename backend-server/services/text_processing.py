b19dd38c3ac51e2c704e70b09d67ba49785c13b7
charantej224 <charantej.career@gmail.com>
2020-10-28 11:30:11 -0400

    front end initial commit

---
+++ backend-server/services/text_processing.py
@@ -0,0 +1,16 @@
+def get_summary_from_bert(input_content):
+    """
+    Name: get_summary_from_bert
+    :param input_content - String
+    :return summary String.
+    """
+    print("bert summary to be defined")
+
+
+def get_answer_for_question(question, content):
+    """
+    Name: get_answer_for_question
+    :param question,content
+    :return summary String.
+    """
+    print("bert question_answering")
