a9ddbef46c75633d8fbb46825c757e19b803a8d8
charantej224 <charantej.career@gmail.com>
2020-10-29 18:08:30 -0400

    header & footer introduction

--- ismart-learn/src/app/app.module.ts
+++ ismart-learn/src/app/app.module.ts
@@ -4,15 +4,23 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CourseOverviewComponent } from './course-overview/course-overview.component';
+import { FormsModule } from '@angular/forms';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CourseOverviewComponent
+    CourseOverviewComponent,
+    HeaderComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule,
+    AppRoutingModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- ismart-learn/src/app/course-overview/course-overview.component.ts
+++ ismart-learn/src/app/course-overview/course-overview.component.ts
@@ -8,8 +8,27 @@ import { Component, OnInit } from '@angular/core';
 export class CourseOverviewComponent implements OnInit {
 
   constructor() { }
+  buttonName = 'enable'
+  disable = true
 
   ngOnInit(): void {
+  }
+
+  getUserFormValues(userform){
+    console.log(userform)
+    // console.log(userform.email)
+    // console.log(userform.password)
+    // console.log(userform.confirmPassword)
+  }
+
+  toggle() {
+    if(this.disable){
+      this.disable = false;
+      this.buttonName = 'disable';
+    } else{
+      this.disable = true;
+      this.buttonName = 'enable';
+    }
   }
 
 }
