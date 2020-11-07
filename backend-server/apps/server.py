560be111f8940c5645692d24371bf8dc8d75c71e
charantej224 <charantej.career@gmail.com>
2020-11-07 17:59:15 -0500

    front end changes

--- backend-server/apps/server.py
+++ backend-server/apps/server.py
@@ -1,3 +1,4 @@
+from flask_cors import CORS, cross_origin
 from flask import Flask, jsonify
 from flask import request
 from services.search_query import search_for_youtube, search_for_google
@@ -5,9 +6,12 @@ from services.scoring_pattern import get_sentence_similarity
 from services.text_processing import get_summary_from_bert, get_answer_for_question
 
 app = Flask(__name__)
+cors = CORS(app)
+app.config['CORS_HEADERS'] = 'Content-Type'
 
 
 @app.route('/api/v1/article/summary', methods=['POST'])
+@cross_origin()
 def get_summary():
     input_request = request.json
     summarized_content = get_summary_from_bert(input_request['content'])
@@ -15,6 +19,7 @@ def get_summary():
 
 
 @app.route('/api/v1/article/answer', methods=['POST'])
+@cross_origin()
 def get_answer():
     input_request = request.json
     answer = get_answer_for_question(input_request['question'], input_request['content'])
@@ -22,6 +27,7 @@ def get_answer():
 
 
 @app.route('/api/v1/article/answer/validate', methods=['POST'])
+@cross_origin()
 def validate_answer():
     input_request = request.json
     answer, score = get_sentence_similarity(input_request['user_answer'], input_request['actual_answer'])
@@ -30,6 +36,7 @@ def validate_answer():
 
 
 @app.route('/api/v1/article/getvideoref', methods=['GET'])
+@cross_origin()
 def get_video_reference():
     query_token = request.args.get('query')
     results = search_for_youtube(query_token)
@@ -37,6 +44,7 @@ def get_video_reference():
 
 
 @app.route('/api/v1/article/getwebref', methods=['GET'])
+@cross_origin()
 def get_web_reference():
     query_token = request.args.get('query')
     results = search_for_google(query_token)
