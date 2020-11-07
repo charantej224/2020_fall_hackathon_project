cc2d520a40f13ff7046e286e17dd79a0e94587e9
charantej224 <charantej.career@gmail.com>
2020-11-07 00:59:39 -0500

    last commit

--- ismart-learn/src/app/course.service.ts
+++ ismart-learn/src/app/course.service.ts
@@ -10,25 +10,33 @@ export class CourseService {
   contentJson: any;
 
   constructor(private httpClient: HttpClient) {
-    if (!this.contentJson) {
-      this.getContentJson().subscribe(data => {
-        this.contentJson = data;
-        console.log(this.contentJson);
-      });
-    }
   }
 
   getContentJson(): Observable<any> {
     return this.httpClient.get('assets/configuration/content.json');
   }
 
-  getServiceData() {
-    let url = 'https://jsonplaceholder.typicode.com/todos/';
-    return this.httpClient.get(url);
-  }
 
   returnContentData() {
     return this.contentJson;
+  }
+
+  // web search links
+  getWebSearch(query:string) {
+    return this.httpClient.get('http://127.0.0.1:5000/api/v1/article/getwebref?query=' + query);
+  }
+
+  // video search links
+  getWebvideo(query:string) {
+    return this.httpClient.get('http://127.0.0.1:5000/api/v1/article/getvideoref?query=' + query);
+  }
+
+  // send abstract content 
+  postAbstarct(content: string) {
+    const headers = { 'content-type': 'application/json'}  
+    return this.httpClient.post('http://127.0.0.1:5000/api/v1/article/summary', content, {'headers':headers}).subscribe(result => {
+      console.log(result);
+    });
   }
 
 }
--- ismart-learn/src/app/module-overview/module-overview.component.ts
+++ ismart-learn/src/app/module-overview/module-overview.component.ts
@@ -1,7 +1,6 @@
-import { Component, OnInit, AfterViewInit, Renderer2, ElementRef } from '@angular/core';
+import { Component, OnInit, AfterViewInit, Renderer2, ElementRef, Input } from '@angular/core';
 import { CourseService } from '../course.service';
 import { ActivatedRoute } from '@angular/router';
-import { mapToMapExpression } from '@angular/compiler/src/render3/util';
 
 @Component({
   selector: 'app-module-overview',
@@ -9,11 +8,19 @@ import { mapToMapExpression } from '@angular/compiler/src/render3/util';
   styleUrls: ['./module-overview.component.css']
 })
 export class ModuleOverviewComponent implements OnInit, AfterViewInit {
-
-  moduleNames: string[] = [];
+ 
+  moduleNames: any[] = [];
   selectedModule: any;
   contentType: string;
   moduleData: any;
+  contentData: any;
+  webLinks: any;
+  webVideoLinks: any;
+  showVideo = false;
+  showWebResult = false;
+  value = "";
+  videovalue = "";
+
   constructor(private courseService: CourseService,
     private renderer: Renderer2,
     private element: ElementRef,
@@ -25,24 +32,77 @@ export class ModuleOverviewComponent implements OnInit, AfterViewInit {
     this.activatedRoute.queryParams.subscribe(params => {
       this.contentType = params.content;
     });
-    const contentData = this.courseService.returnContentData();
-    this.moduleData = contentData[this.contentType].modules
-    console.log(this.moduleData)
-    Object.keys(this.moduleData).forEach((key,index) => {
-      console.log(key)
-      console.log(this.moduleData[key])
-      this.moduleNames.push(this.moduleData[key]['name'])
-  });
+
+    this.courseService.getContentJson().subscribe(data => {
+      this.contentData = data;
+      this.moduleData = this.contentData[this.contentType].modules
+      Object.keys(this.moduleData).forEach((key, index) => {
+        console.log(key);
+        console.log(this.moduleData[key]);
+        let moduleName = {
+          'key':key,
+          'name':this.moduleData[key]['name']
+        };
+        this.moduleNames.push(moduleName);
+      });
+    })
   }
 
   ngAfterViewInit() {
     const submit = this.element.nativeElement.querySelector('#submit');
     this.renderer.listen(submit, 'click', (event) => {
-      console.log("heloo babe");
+      console.log(this.selectedModule);
     });
   }
+
+  _keyUp(event: any) {
+    const pattern = /[0-9\+\-\ ]/;
+    let inputChar = String.fromCharCode(event.key);
+
+    if (!pattern.test(inputChar)) {
+      // invalid character, prevent input
+      event.preventDefault();
+    }
+}
+
+  searchWeb(event) {
+    console.log(event);
+    this.courseService.getWebSearch(event).subscribe(result => {
+      if (result !== null && typeof result !== 'undefined') {
+        this.webLinks = result;
+        this.showWebResult = true;
+      }
+    });
+  }
+
+  searchVideo(query) {
+    this.showVideo = true;
+    this.courseService.getWebvideo(query).subscribe(video => {
+      if (video !==null && typeof video !== 'undefined') {
+        this.webVideoLinks = video;
+      }
+    });
+  }
+
+  sendContent(content) {
+    if (content !== null && typeof content !== 'undefined') {
+      this.courseService.postAbstarct(content);
+    }
+    
+  }
+
+  removeWebResults(closediv) {
+    if (closediv === 'link') {
+      this.showWebResult = false;
+    } else {
+      this.showVideo = false;
+    }
+    
+  }
+
   openModule(moduleKey) {
-    this.selectedModule = this.moduleData[moduleKey]
+    this.selectedModule = this.moduleData[moduleKey];
+    console.log(this.selectedModule)
   }
 
 }
